cb05f38134aaac3965c181d602f49a336aec2845
rfortiz <raphael.ortiz@protonmail.ch>
2018-01-29 22:29:09 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Project title
+
+project description
+
+
+![Alt text](relative/path/to/img.jpg?raw=true "Title")
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='cryptoledger',
+    description='Consolidate transactions history from cryptocurrency exchanges, calculate portfolio and generate a pdf report',
+    url='https://github.com/...',
+    author='Raphael Ortiz',
+    author_email='raphael.ortiz@protonmail.ch',
+    packages=['cryptoledger'],
+    version='0.1',
+    license='MIT',
+)
